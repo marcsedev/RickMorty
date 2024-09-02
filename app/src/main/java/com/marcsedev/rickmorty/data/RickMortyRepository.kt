7ca9ca490bd03626829c022fd2eb2b4bbf8b4cad
Marc Sogas Esteve <msogas@almex.es>
2024-09-02 17:56:56 +0200

    WIP: prepare for add paging

--- app/src/main/java/com/marcsedev/rickmorty/data/RickMortyRepository.kt
+++ app/src/main/java/com/marcsedev/rickmorty/data/RickMortyRepository.kt
@@ -1,6 +1,13 @@
 package com.marcsedev.rickmorty.data
 
+import androidx.paging.PagingData
+import com.marcsedev.rickmorty.presentation.model.CharacterModel
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class RickMortyRepository @Inject constructor(api:RickMortyApiService) {
+class RickMortyRepository @Inject constructor(val api:RickMortyApiService) {
+
+    fun getAllCharacters(): Flow<PagingData<CharacterModel>>{
+        
+    }
 }
