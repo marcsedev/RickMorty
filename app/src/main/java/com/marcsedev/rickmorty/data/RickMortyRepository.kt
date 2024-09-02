1ac3fc735869f85df2e45f53971985b0b6ce658e
Marc Sogas Esteve <msogas@almex.es>
2024-09-02 18:15:55 +0200

    Add pagination configuration PagingSource

--- app/src/main/java/com/marcsedev/rickmorty/data/RickMortyRepository.kt
+++ app/src/main/java/com/marcsedev/rickmorty/data/RickMortyRepository.kt
@@ -1,5 +1,7 @@
 package com.marcsedev.rickmorty.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.marcsedev.rickmorty.presentation.model.CharacterModel
 import kotlinx.coroutines.flow.Flow
@@ -7,7 +9,17 @@ import javax.inject.Inject
 
 class RickMortyRepository @Inject constructor(val api:RickMortyApiService) {
 
+    companion object {
+        const val MAX_ITEMS = 20
+        const val PREFETCH_ITEMS = 3
+
+    }
+
     fun getAllCharacters(): Flow<PagingData<CharacterModel>>{
-        
+        return Pager(
+            config = PagingConfig(pageSize = MAX_ITEMS, prefetchDistance = PREFETCH_ITEMS),
+            pagingSourceFactory = { CharacterPagingSource(api) }
+        ).flow
+
     }
 }
