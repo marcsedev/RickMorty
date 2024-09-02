2f908cc167be123e4c66580a219c09d830240cf5
Marc Sogas Esteve <msogas@almex.es>
2024-09-02 17:50:39 +0200

    Add di module

--- app/src/main/java/com/marcsedev/rickmorty/data/RickMortyApiService.kt
+++ app/src/main/java/com/marcsedev/rickmorty/data/RickMortyApiService.kt
@@ -1,12 +1,13 @@
 package com.marcsedev.rickmorty.data
 
 import com.marcsedev.rickmorty.data.response.CharacterResponse
+import com.marcsedev.rickmorty.data.response.ResponseWrapper
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface RickMortyApiService {
-    @GET("character")
-    suspend fun getCharacters(@Query("page") page: Int): Response<CharacterResponse>
+    @GET("/character/")
+    suspend fun getCharacters(@Query("page") page: Int): ResponseWrapper
 
 }
---
+++ app/src/main/java/com/marcsedev/rickmorty/data/RickMortyRepository.kt
@@ -0,0 +1,6 @@
+package com.marcsedev.rickmorty.data
+
+import javax.inject.Inject
+
+class RickMortyRepository @Inject constructor(api:RickMortyApiService) {
+}
