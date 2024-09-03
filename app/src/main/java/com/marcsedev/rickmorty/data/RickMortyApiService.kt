069b883865ab35feeb6b97f4de99e3a6a52f9830
Marc Sogas Esteve <msogas@almex.es>
2024-09-03 10:07:35 +0200

    WIP: show characters

--- app/src/main/java/com/marcsedev/rickmorty/data/RickMortyApiService.kt
+++ app/src/main/java/com/marcsedev/rickmorty/data/RickMortyApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface RickMortyApiService {
-    @GET("/character/")
+    @GET("/api/character/")
     suspend fun getCharacters(@Query("page") page: Int): ResponseWrapper
 
 }
